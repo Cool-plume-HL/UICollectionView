ce30e73e558a431fffb8ec72b3b36b95018e58d9
快医科技 <HeLin@kuaiyikeji-MacBook-Pro.local>
2017-11-16 10:13:52 +0800

    Initial Commit

---
+++ ExploringCollectionView/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  ExploringCollectionView
+//
+//  Created by HeLin KYKJ on 2017/11/16.
+//  Copyright © 2017年 CDFGJ. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
---
+++ ExploringCollectionView/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ExploringCollectionView
+//
+//  Created by HeLin KYKJ on 2017/11/16.
+//  Copyright © 2017年 CDFGJ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
