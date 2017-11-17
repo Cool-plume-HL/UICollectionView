617d9b1a311c1704d0c6dc60b7148820f9a607eb
HeLin KYKJ <HeLin@kuaiyikeji-MacBook-Pro.local>
2017-11-17 15:39:44 +0800

    first commit

---
+++ ExploringCollectionView/New Group/HLCardCell.h
@@ -0,0 +1,13 @@
+//
+//  KYPatientIDCardCell.h
+//  Mom
+//
+//  Created by HeLin KYKJ on 2017/10/21.
+//  Copyright © 2017年 快医科技. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface KYPatientIDCardCell : UICollectionViewCell
+@end
+
